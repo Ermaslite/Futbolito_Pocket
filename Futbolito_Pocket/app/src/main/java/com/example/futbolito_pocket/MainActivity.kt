db4971f8f706d8268398e83db99d1fa9dad186e1
Erik Rodriguez <98299098+Ermaslite@users.noreply.github.com>
2026-04-27 22:24:35 -0600

    Diseño de la cancha
    Diseño de la cancha lo mas parecido posible

--- Futbolito_Pocket/app/src/main/java/com/example/futbolito_pocket/MainActivity.kt
+++ Futbolito_Pocket/app/src/main/java/com/example/futbolito_pocket/MainActivity.kt
@@ -4,13 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import com.example.futbolito_pocket.ui.accelerometer.FootballFieldCanvas
 import com.example.futbolito_pocket.ui.theme.Futbolito_PocketTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +22,19 @@ class MainActivity : ComponentActivity() {
         setContent {
             Futbolito_PocketTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Box(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxSize()
+                            .background(Color(0xFF4CAF50)) // Un color verde para el campo
+                    ) {
+                        FootballFieldCanvas(
+                            goalWidth = 300f,
+                            goalHeight = 100f
+                        )
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Futbolito_PocketTheme {
-        Greeting("Android")
     }
 }
